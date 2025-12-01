8a83c5b04bda6b67d0c283bcc5f6b07afdf8604c
wangruoshui <16633445526@163.com>
2025-12-01 16:40:28 +0800

    基于Stream消息队列实现异步秒杀

--- src/main/resources/seckill.lua
+++ src/main/resources/seckill.lua
@@ -2,7 +2,8 @@
 local voucherId = ARGV[1]
 -- 1.2.用户id
 local userId = ARGV[2]
-
+--1.3订单id
+local orderId=ARGV[3]
 -- 2.数据key
 -- 2.1.库存key
 local stockKey = 'seckill:stock:' .. voucherId
@@ -29,4 +30,6 @@ end
 redis.call('incrby', stockKey, -1)
 -- 3.5.下单（保存用户）sadd orderKey userId
 redis.call('sadd', orderKey, userId)
+--3.6发送消息到队列中，XADD stream.orders * k1 v2 k2 v2 ..
+redis.call('xadd','stream.orders','*','userId',userId,'voucherId',voucherId,'Id',orderId)
 return 0
